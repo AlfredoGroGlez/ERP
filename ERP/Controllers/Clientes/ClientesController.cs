c7ff557ca5a5378278946ce5f9fc4272925226a5
Alfredo_Guerrero <alfredo77385@gmail.com>
2021-10-18 19:00:37 -0500

    Controlador de clientes

---
+++ ERP/Controllers/Clientes/ClientesController.cs
@@ -0,0 +1,18 @@
+﻿using ERP.Filter;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERP.Controllers.Clientes
+{
+    public class ClientesController : Controller
+    {
+        [ServiceFilter(typeof(FilterSeguridad))]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
